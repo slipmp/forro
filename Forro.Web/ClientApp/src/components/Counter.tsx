5af6297f9594ab839006893dc5cf062f42ff6678
Marcos Soares <slipmp@gmail.com>
2020-01-06 23:04:16 -0800

    Fixing broken layout from 2 other pages (Counter and FetchData)

--- Forro.Web/ClientApp/src/components/Counter.tsx
+++ Forro.Web/ClientApp/src/components/Counter.tsx
@@ -13,17 +13,36 @@ class Counter extends React.PureComponent<CounterProps> {
     public render() {
         return (
             <React.Fragment>
-                <h1>Counter</h1>
 
-                <p>This is a simple example of a React component.</p>
+                <main role="main">
+                    <article>
+                        <header className="section-top-padding background-white">
+                            <div className="line text-center">
+                                <h1 className="text-dark text-s-size-30 text-m-size-40 text-l-size-headline text-thin text-line-height-1">
+                                    Counter
+                                </h1>
+                                <p className="margin-bottom-0 text-size-16 text-dark">
+                                    This is a simple example of a React component.
+                                </p>
+                            </div>
+                        </header>
+                        <section className="section-top-padding background-white">
+                            <div className="line">
+                                <div className="margin">
+                                    <p aria-live="polite">Current count: <strong>{this.props.count}</strong></p>
 
-                <p aria-live="polite">Current count: <strong>{this.props.count}</strong></p>
+                                    <button type="button"
+                                        className="btn btn-primary btn-lg"
+                                        onClick={() => { this.props.increment(); }}>
+                                        Increment
+                                    </button>
+                                </div>
+                            </div>
+                            <br />
+                        </section>
+                    </article>
+                </main>
 
-                <button type="button"
-                    className="btn btn-primary btn-lg"
-                    onClick={() => { this.props.increment(); }}>
-                    Increment
-                </button>
             </React.Fragment>
         );
     }
--- Forro.Web/ClientApp/src/components/FetchData.tsx
+++ Forro.Web/ClientApp/src/components/FetchData.tsx
@@ -7,78 +7,97 @@ import * as WeatherForecastsStore from '../store/WeatherForecasts';
 
 // At runtime, Redux will merge together...
 type WeatherForecastProps =
-  WeatherForecastsStore.WeatherForecastsState // ... state we've requested from the Redux store
-  & typeof WeatherForecastsStore.actionCreators // ... plus action creators we've requested
-  & RouteComponentProps<{ startDateIndex: string }>; // ... plus incoming routing parameters
+    WeatherForecastsStore.WeatherForecastsState // ... state we've requested from the Redux store
+    & typeof WeatherForecastsStore.actionCreators // ... plus action creators we've requested
+    & RouteComponentProps<{ startDateIndex: string }>; // ... plus incoming routing parameters
 
 
 class FetchData extends React.PureComponent<WeatherForecastProps> {
-  // This method is called when the component is first added to the document
-  public componentDidMount() {
-    this.ensureDataFetched();
-  }
+    // This method is called when the component is first added to the document
+    public componentDidMount() {
+        this.ensureDataFetched();
+    }
 
-  // This method is called when the route parameters change
-  public componentDidUpdate() {
-    this.ensureDataFetched();
-  }
+    // This method is called when the route parameters change
+    public componentDidUpdate() {
+        this.ensureDataFetched();
+    }
 
-  public render() {
-    return (
-      <React.Fragment>
-        <h1 id="tabelLabel">Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server and working with URL parameters.</p>
-        {this.renderForecastsTable()}
-        {this.renderPagination()}
-      </React.Fragment>
-    );
-  }
+    public render() {
+        return (
+            <React.Fragment>
+                <main role="main">
+                    <article>
+                        <header className="section-top-padding background-white">
+                            <div className="line text-center">
+                                <h1 className="text-dark text-s-size-30 text-m-size-40 text-l-size-headline text-thin text-line-height-1">
+                                    Weather forecast
+                            </h1>
+                                <p className="margin-bottom-0 text-size-16 text-dark">
+                                    This component demonstrates fetching data from the server and working with URL parameters.
+                            </p>
+                            </div>
+                        </header>
+                        <section className="section-top-padding background-white">
+                            <div className="line">
+                                <div className="margin">
+                                    {this.renderForecastsTable()}
+                                    {this.renderPagination()}
+                                </div>
+                            </div>
+                            <br />
+                        </section>
+                    </article>
+                </main>
+            </React.Fragment>
+        );
+    }
 
-  private ensureDataFetched() {
-    const startDateIndex = parseInt(this.props.match.params.startDateIndex, 10) || 0;
-    this.props.requestWeatherForecasts(startDateIndex);
-  }
+    private ensureDataFetched() {
+        const startDateIndex = parseInt(this.props.match.params.startDateIndex, 10) || 0;
+        this.props.requestWeatherForecasts(startDateIndex);
+    }
 
-  private renderForecastsTable() {
-    return (
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.forecasts.map((forecast: WeatherForecastsStore.WeatherForecast) =>
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
-  }
+    private renderForecastsTable() {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Temp. (C)</th>
+                        <th>Temp. (F)</th>
+                        <th>Summary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.forecasts.map((forecast: WeatherForecastsStore.WeatherForecast) =>
+                        <tr key={forecast.date}>
+                            <td>{forecast.date}</td>
+                            <td>{forecast.temperatureC}</td>
+                            <td>{forecast.temperatureF}</td>
+                            <td>{forecast.summary}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
 
-  private renderPagination() {
-    const prevStartDateIndex = (this.props.startDateIndex || 0) - 5;
-    const nextStartDateIndex = (this.props.startDateIndex || 0) + 5;
+    private renderPagination() {
+        const prevStartDateIndex = (this.props.startDateIndex || 0) - 5;
+        const nextStartDateIndex = (this.props.startDateIndex || 0) + 5;
 
-    return (
-      <div className="d-flex justify-content-between">
-        <Link className='btn btn-outline-secondary btn-sm' to={`/fetch-data/${prevStartDateIndex}`}>Previous</Link>
-        {this.props.isLoading && <span>Loading...</span>}
-        <Link className='btn btn-outline-secondary btn-sm' to={`/fetch-data/${nextStartDateIndex}`}>Next</Link>
-      </div>
-    );
-  }
+        return (
+            <div className="d-flex justify-content-between">
+                <Link className='btn btn-outline-secondary btn-sm' to={`/fetch-data/${prevStartDateIndex}`}>Previous</Link>
+                {this.props.isLoading && <span>Loading...</span>}
+                <Link className='btn btn-outline-secondary btn-sm' to={`/fetch-data/${nextStartDateIndex}`}>Next</Link>
+            </div>
+        );
+    }
 }
 
 export default connect(
-  (state: ApplicationState) => state.weatherForecasts, // Selects which state properties are merged into the component's props
-  WeatherForecastsStore.actionCreators // Selects which action creators are merged into the component's props
+    (state: ApplicationState) => state.weatherForecasts, // Selects which state properties are merged into the component's props
+    WeatherForecastsStore.actionCreators // Selects which action creators are merged into the component's props
 )(FetchData as any);
